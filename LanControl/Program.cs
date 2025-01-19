2791ab30bc092a160f1625e16aa552b6afb1527f
Luiz Henrique <luizhen428@gmail.com>
2025-01-18 21:23:56 -0300

    Add: Session authorization and requirement

--- LanControl/Program.cs
+++ LanControl/Program.cs
@@ -6,9 +6,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddCore();
-
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(x => x.IdleTimeout = TimeSpan.FromDays(1));
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.MapControllers();
-app.Run();
+app.UseSession();
+await app.RunAsync();
